fec3a1e5ce187c7e5f3895665780cc8746ed59f6
Swissfmprofessionals <admin@swissfmprofessionals.ch>
2025-10-19 22:36:16 +0200

    Add files via upload

---
+++ landing-page/Hermes2022_Zustände.js
@@ -0,0 +1,207 @@
+const ZustaendeData = [
+  {
+    "Nr-ID": "1",
+    "Zustand": "Altsystem entfernt",
+    "Originalbeschreibung": "Die alte Systemversion sowie das Altsystem sind unter Berücksichtigung der Vorgaben abgebaut bzw. entfernt. Die Ausserbetriebssetzung umfasst ebenfalls die Vernichtung oder Archivierung von Daten."
+  },
+  {
+    "Nr-ID": "3",
+    "Zustand": "Betrieb aktiviert",
+    "Originalbeschreibung": "Der Betrieb des aktivierten Systems ist aufgenommen und erfolgt mit der im Betriebskonzept definierten Betriebsorganisation. Die im Betriebshandbuch festgelegten Aktivitäten werden ausgeführt. Das Betriebspersonal nimmt die Betriebsaufgaben wahr. Die Voraussetzungen für die Messung und Einhaltung des SLAs sind gegeben."
+  },
+  {
+    "Nr-ID": "4",
+    "Zustand": "Betriebsinfrastruktur realisiert",
+    "Originalbeschreibung": "Die Betriebsinfrastruktur umfasst alle für die Erstellung und den Betrieb eines Systems benötigten Infrastrukturen mit den verschiedenen Systemumgebungen (Entwicklung, Test, Produktion usw.) und allen ihren Komponenten."
+  },
+  {
+    "Nr-ID": "5",
+    "Zustand": "Betriebsorganisation realisiert",
+    "Originalbeschreibung": "Die im Betriebskonzept definierte Betriebsorganisation mit der Betriebsaufbauorganisation und den Betriebsprozessen des Betreibers sind realisiert. Das Betriebspersonal ist ausgebildet und kann die Betriebsaufgaben wahrnehmen."
+  },
+  {
+    "Nr-ID": "6",
+    "Zustand": "Einführungsmassnahmen durchgeführt",
+    "Originalbeschreibung": "Die im Einführungskonzept beschriebenen und realisierten Massnahmen sind durchgeführt. Die Umsetzung der Massnahmen ist überprüft und deren Qualitätssicherung durchgeführt."
+  },
+  {
+    "Nr-ID": "7",
+    "Zustand": "Einführungsmassnahmen realisiert",
+    "Originalbeschreibung": "Die im Einführungskonzept beschriebenen Massnahmen und die für die Einführung nötige Organisation sind realisiert."
+  },
+  {
+    "Nr-ID": "8",
+    "Zustand": "ISDS-Konzept überführt",
+    "Originalbeschreibung": "Das ISDS-Konzept wurde aktualisiert, durch die Controlling- und Vorgabestelle geprüft und von der Projektorganisation in die Stammorganisation überführt."
+  },
+  {
+    "Nr-ID": "9",
+    "Zustand": "ISDS-Massnahmen realisiert",
+    "Originalbeschreibung": "Die ISDS-Massnahmen wurden auf der Grundlage des ISDS-Konzepts realisiert. Sie stellen sicher, dass die Anforderungen an den Schutzbedarf gemäss ISDS-Konzept erfüllt werden."
+  },
+  {
+    "Nr-ID": "10",
+    "Zustand": "Meilenstein Abnahme",
+    "Originalbeschreibung": "Der Meilenstein wird erreicht, wenn der Entscheid Abnahme getroffen wird. Die Lösung wird definitiv in die Anwendungs- und allenfalls auch in die Betriebsorganisation überführt."
+  },
+  {
+    "Nr-ID": "11",
+    "Zustand": "Meilenstein Abnahme Migration",
+    "Originalbeschreibung": "Der Meilenstein wird erreicht, wenn der Entscheid Abnahme Migration getroffen wird. Die Nutzung des neuen Systems kann für die Anwender freigegeben werden (Entscheid Betriebsaufnahme)."
+  },
+  {
+    "Nr-ID": "12",
+    "Zustand": "Meilenstein Ausschreibung",
+    "Originalbeschreibung": "Der Meilenstein wird erreicht, wenn der Entscheid Ausschreibung getroffen wird. Die Ausschreibung kann publiziert werden."
+  },
+  {
+    "Nr-ID": "13",
+    "Zustand": "Meilenstein Betriebsaufnahme",
+    "Originalbeschreibung": "Der Meilenstein wird erreicht, wenn der Entscheid Betriebsaufnahme getroffen wird. Die Lösung kann in Betrieb genommen werden."
+  },
+  {
+    "Nr-ID": "14",
+    "Zustand": "Meilenstein Durchführungsfreigabe",
+    "Originalbeschreibung": "Der Meilenstein wird erreicht, wenn der Entscheid Durchführungsfreigabe getroffen wird. Die Arbeiten starten gemäss dem Durchführungsauftrag."
+  },
+  {
+    "Nr-ID": "15",
+    "Zustand": "Meilenstein ISDS-Konzept",
+    "Originalbeschreibung": "Der Meilenstein wird erreicht, wenn der Entscheid ISDS-Konzept getroffen wird. Die Realisierung der ISDS-Massnahmen kann an die Hand genommen werden."
+  },
+  {
+    "Nr-ID": "16",
+    "Zustand": "Meilenstein Lösungsarchitektur",
+    "Originalbeschreibung": "Der Meilenstein wird erreicht, wenn der Entscheid Lösungsarchitektur getroffen wird. Dies bildet die Voraussetzung für die Entwicklung oder Adaption von Systemen."
+  },
+  {
+    "Nr-ID": "17",
+    "Zustand": "Meilenstein Phasenfreigabe",
+    "Originalbeschreibung": "Der Meilenstein wird erreicht, wenn der Entscheid Phasenfreigabe getroffen wird. Die Arbeiten in der nächsten Phase können gestartet werden."
+  },
+  {
+    "Nr-ID": "18",
+    "Zustand": "Meilenstein Phasenfreigabe Abschluss",
+    "Originalbeschreibung": "Der Meilenstein wird erreicht, wenn der Entscheid Phasenfreigabe Abschluss getroffen wird. Die Leistungserbringung im Rahmen der Lösungsentstehung ist beendet."
+  },
+  {
+    "Nr-ID": "19",
+    "Zustand": "Meilenstein Produktkonzept",
+    "Originalbeschreibung": "Der Meilenstein wird erreicht, wenn der Entscheid Produktkonzept getroffen wird. Das Konzept ist die Voraussetzung für die Entwicklung oder Adaption von Produkten oder Dienstleistungen."
+  },
+  {
+    "Nr-ID": "20a/20b",
+    "Zustand": "Meilenstein Projektabschluss",
+    "Originalbeschreibung": "Der Meilenstein wird erreicht, wenn der Entscheid Projektabschluss getroffen wird. Das Projekt ist beendet."
+  },
+  {
+    "Nr-ID": "21",
+    "Zustand": "Meilenstein Projektinitialisierungsfreigabe",
+    "Originalbeschreibung": "Der Meilenstein wird erreicht, wenn der Entscheid Projektinitialisierungsfreigabe getroffen wird. Das Projekt wurde formell ins Leben gerufen."
+  },
+  {
+    "Nr-ID": "23",
+    "Zustand": "Meilenstein Releasefreigabe",
+    "Originalbeschreibung": "Der Meilenstein wird erreicht, wenn der Entscheid Releasefreigabe getroffen wird. Im Rahmen der agilen Vorgehensweise können die Arbeiten am nächsten Release starten."
+  },
+  {
+    "Nr-ID": "24",
+    "Zustand": "Meilenstein Vorabnahme",
+    "Originalbeschreibung": "Der Meilenstein wird erreicht, wenn der Entscheid Vorabnahme getroffen wird. Dies bildet die Grundlage für die Durchführung der Einführungsmassnahmen und die Betriebsaufnahme."
+  },
+  {
+    "Nr-ID": "25",
+    "Zustand": "Meilenstein Weiteres Vorgehen",
+    "Originalbeschreibung": "Der Meilenstein wird erreicht, wenn der Entscheid Weiteres Vorgehen getroffen wird. Dies bildet die Grundlage für die Erarbeitung des Projektmanagementplans sowie des Durchführungsauftrags."
+  },
+  {
+    "Nr-ID": "26",
+    "Zustand": "Meilenstein Zuschlag",
+    "Originalbeschreibung": "Der Meilenstein wird erreicht, wenn der Entscheid Zuschlag getroffen wird. Der Entscheid kann publiziert und die Vertragsarbeiten mit dem Zuschlagsempfänger gestartet werden."
+  },
+  {
+    "Nr-ID": "27",
+    "Zustand": "Migration durchgeführt",
+    "Originalbeschreibung": "Die Migration vom alten auf das neue bzw. weiterentwickelte System ist durchgeführt und gemäss den Vorgaben der Controlling- und Koordinationsstellen dokumentiert."
+  },
+  {
+    "Nr-ID": "28",
+    "Zustand": "Migrationsverfahren realisiert",
+    "Originalbeschreibung": "Das realisierte Migrationsverfahren ist durch den Entwickler bzw. den Tester des Erstellers getestet worden."
+  },
+  {
+    "Nr-ID": "29",
+    "Zustand": "Organisation aktiviert",
+    "Originalbeschreibung": "Die neue Organisation ist aktiviert. Sie führt ihre Prozesse gemäss den Prozessbeschreibungen durch."
+  },
+  {
+    "Nr-ID": "30",
+    "Zustand": "Organisation umgesetzt",
+    "Originalbeschreibung": "Die im Organisationskonzept definierte Organisation ist umgesetzt."
+  },
+  {
+    "Nr-ID": "31",
+    "Zustand": "Produkt aktiviert",
+    "Originalbeschreibung": "Das aktivierte Produkt wird den Anwendern für die Nutzung zur Verfügung gestellt."
+  },
+  {
+    "Nr-ID": "32",
+    "Zustand": "Produkt entwickelt oder angepasst",
+    "Originalbeschreibung": "Das entwickelte und/oder angepasste Produkt ist durch den Entwickler bzw. den Tester des Erstellers getestet worden."
+  },
+  {
+    "Nr-ID": "33a/33b/33c",
+    "Zustand": "Prototyp realisiert",
+    "Originalbeschreibung": "Mit dem Prototyp wird die Machbarkeit oder das Verhalten eines Systems bzw. eines Produkts in einer bestimmten Situation geprüft."
+  },
+  {
+    "Nr-ID": "34",
+    "Zustand": "Schnittstellen realisiert",
+    "Originalbeschreibung": "Die realisierten Schnittstellen stellen den Datenaustausch zwischen dem System und den Umsystemen sicher."
+  },
+  {
+    "Nr-ID": "35",
+    "Zustand": "System aktiviert",
+    "Originalbeschreibung": "Das aktivierte System wird den Anwendern für die Nutzung zur Verfügung gestellt."
+  },
+  {
+    "Nr-ID": "36",
+    "Zustand": "System entwickelt oder parametrisiert",
+    "Originalbeschreibung": "Das entwickelte und/oder parametrisierte System ist durch den Entwickler bzw. den Integrator und den Tester des Erstellers getestet worden."
+  },
+  {
+    "Nr-ID": "37",
+    "Zustand": "System integriert",
+    "Originalbeschreibung": "Das integrierte System steht den Testern des Anwenders für die Tests und die Vorabnahme zur Verfügung."
+  },
+  {
+    "Nr-ID": "40",
+    "Zustand": "Testinfrastruktur realisiert",
+    "Originalbeschreibung": "Für die Durchführung der Tests wird eine Testinfrastruktur bestehend aus Testsystem (darunter sind alle Systeme zur Testdurchführung zu verstehen), Testdaten und Testhilfsmitteln verwendet."
+  },
+  {
+    "Nr-ID": "41",
+    "Zustand": "Testinfrastruktur überführt",
+    "Originalbeschreibung": "Die gesamte Testinfrastruktur inklusive des Testkonzepts wurde in die Stammorganisation überführt."
+  },
+  {
+    "Nr-ID": "38",
+    "Zustand": "Teilprojekt geplant",
+    "Originalbeschreibung": "Der Teilprojektauftrag ist vom Projektleiter freigegeben und vom Teilprojektleiter angenommen. Die Rahmenbedingungen für das Teilprojekt sind definiert und kommuniziert."
+  },
+  {
+    "Nr-ID": "39",
+    "Zustand": "Teilprojekt initialisiert",
+    "Originalbeschreibung": "Der Teilprojektmanagementplan ist erstellt und vom Projektleiter genehmigt. Die detaillierte Planung für das Teilprojekt liegt vor."
+  },
+  {
+    "Nr-ID": "22",
+    "Zustand": "Meilenstein Teilprojektabschluss",
+    "Originalbeschreibung": "Ein klar definiertes Arbeitspaket innerhalb eines (Teil-)Projekts wurde vom (Teil-)Projektleiter zur Bearbeitung freigegeben. Die ausführenden Rollen können mit der Arbeit beginnen."
+  },
+  {
+    "Nr-ID": "2",
+    "Zustand": "Arbeitspaket freigegeben",
+    "Originalbeschreibung": "Der Meilenstein wird erreicht, wenn der Entscheid Teilprojektabschluss durch den Projektleiter getroffen wird. Das Teilprojekt ist formell beendet."
+  }
+];
